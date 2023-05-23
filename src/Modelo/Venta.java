d302a36df272d43a5cd9d4f1b9c67793c8c56910
Renat0Espinoza <131412695+Renat0Espinoza@users.noreply.github.com>
2023-05-23 18:45:45 -0400

    creacion venta

--- src/Modelo/Venta.java
+++ src/Modelo/Venta.java
@@ -1,6 +1,27 @@
 package Modelo;
 
 public class Venta {
-    private String codVenta;
+    private long codigoVenta;
     private String fechaVenta;
+
+    public Venta(long codigoVenta, String fechaVenta) {
+        this.codigoVenta = codigoVenta;
+        this.fechaVenta = fechaVenta;
+    }
+
+    public long getCodigoVenta() {
+        return codigoVenta;
+    }
+
+    public void setCodigoVenta(long codigoVenta) {
+        this.codigoVenta = codigoVenta;
+    }
+
+    public String getFechaVenta() {
+        return fechaVenta;
+    }
+
+    public void setFechaVenta(String fechaVenta) {
+        this.fechaVenta = fechaVenta;
+    }
 }
