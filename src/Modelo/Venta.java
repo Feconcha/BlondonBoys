11edf58edd6ffb5b282b52f512d014b6b316db16
Feconcha <felipe.concha2201@alumnos.ubiobio.cl>
2023-05-23 23:41:31 -0400

    Modifica clase Venta

--- src/Modelo/Venta.java
+++ src/Modelo/Venta.java
@@ -1,12 +1,18 @@
 package Modelo;
 
+import java.util.ArrayList;
+
 public class Venta {
     private long codigoVenta;
     private String fechaVenta;
+    private ArrayList<Producto> listaProductos;
+    private Cliente cliente;
 
-    public Venta(long codigoVenta, String fechaVenta) {
+    public Venta(long codigoVenta, String fechaVenta, ArrayList<Producto> listaProductos, Cliente cliente) {
         this.codigoVenta = codigoVenta;
         this.fechaVenta = fechaVenta;
+        this.listaProductos = listaProductos;
+        this.cliente = cliente;
     }
 
     public long getCodigoVenta() {
@@ -23,5 +29,21 @@ public class Venta {
 
     public void setFechaVenta(String fechaVenta) {
         this.fechaVenta = fechaVenta;
+    }
+
+    public ArrayList<Producto> getListaProductos() {
+        return listaProductos;
+    }
+
+    public void setListaProductos(ArrayList<Producto> listaProductos) {
+        this.listaProductos = listaProductos;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
     }
 }
