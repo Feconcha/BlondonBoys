c19090d93f3b52d15496f77da06f02de955ffd72
Feconcha <felipe.concha2201@alumnos.ubiobio.cl>
2023-05-28 21:13:02 -0400

    modifica metodos de fechaVenta

--- src/Modelo/Venta.java
+++ src/Modelo/Venta.java
@@ -3,19 +3,19 @@ package Modelo;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Venta {
     private long codigoVenta;
-    private String fechaVenta;
+    private LocalDate fechaVenta;
     private Cliente cliente;
     private ArrayList<Producto>productos;
-    private LocalDate fechaVenta;
 
-    public Venta(long codigoVenta, String fechaVenta, Cliente cliente) {
+    public Venta(long codigoVenta, LocalDate fechaVenta, Cliente cliente) {
         this.codigoVenta = codigoVenta;
         this.fechaVenta = fechaVenta;
         this.cliente = cliente;
-        productos= new ArrayList<>();
+        productos = new ArrayList<>();
     }
 
     public long getCodigoVenta() {
@@ -26,41 +26,32 @@ public class Venta {
         this.codigoVenta = codigoVenta;
     }
 
-    public String getFechaVenta() {
+    public LocalDate getFechaVenta() {
         return fechaVenta;
     }
 
-    public void setFechaVenta(String fechaVenta) {
+    public void setFechaVenta(LocalDate fechaVenta) {
         this.fechaVenta = fechaVenta;
     }
+
     public void addProductos(Producto producto){
         productos.add(producto);
-    }
-    public int getVentas(){
-        return productos.size();
-    }
-    public String toString(){
-        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        return codigoVenta + ";" + cliente.getRut() + ";" + fechaVenta.format(formato);
     }
 
     public Cliente getCliente() {
         return cliente;
     }
 
+    public int getVentas(){
+        return productos.size();
+    }
+
     public void setCliente(Cliente cliente) {
         this.cliente = cliente;
     }
 
-    public ArrayList<Producto> getProductos() {
-        return productos;
-    }
-
-    public void setProductos(ArrayList<Producto> productos) {
-        this.productos = productos;
-    }
-
-    public void setFechaVenta(LocalDate fechaVenta) {
-        this.fechaVenta = fechaVenta;
+    public String toString(){
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        return codigoVenta + ";" + cliente.getRut() + ";" + fechaVenta.format(formato);
     }
 }
