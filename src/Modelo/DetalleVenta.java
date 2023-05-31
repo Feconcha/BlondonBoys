6215e1d9070db25bc6140c21655fccc4d680079b
Renat0Espinoza <131412695+Renat0Espinoza@users.noreply.github.com>
2023-05-31 09:57:05 -0400

    Creacion detalle venta

---
+++ src/Modelo/DetalleVenta.java
@@ -0,0 +1,22 @@
+package Modelo;
+
+public class DetalleVenta {
+    private int cantidad;
+    private Producto producto;
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+}
--- src/Modelo/Venta.java
+++ src/Modelo/Venta.java
@@ -1,49 +1,37 @@
 package Modelo;
 
+import javax.lang.model.type.DeclaredType;
 import java.util.ArrayList;
 
 public class Venta {
     private long codigoVenta;
     private String fechaVenta;
     private Cliente cliente;
-    private ArrayList<Producto> productos;
+    private ArrayList<DetalleVenta> DetalleVenta;
 
-    public Venta(long codigoVenta, String fechaVenta, Cliente cliente, ArrayList<Producto> productos) {
+    public Venta(long codigoVenta, String fechaVenta, Cliente cliente, ArrayList<DetalleVenta> detalleVenta) {
         this.codigoVenta = codigoVenta;
         this.fechaVenta = fechaVenta;
         this.cliente = cliente;
-        this.productos = productos;
+        DetalleVenta = detalleVenta;
     }
 
-    public long getCodigoVenta() {
-        return codigoVenta;
-    }
+    public void dibujaFactura(){
+        System.out.println("Codigo de factira: "+codigoVenta);
+        System.out.println("Nombre del cliente: "+cliente.toString());
+        System.out.println("Fecha de venta: "+fechaVenta);
+        System.out.println("El detalle de lo vendido es: ");
+        int acumulado=0;
 
-    public void setCodigoVenta(long codigoVenta) {
-        this.codigoVenta = codigoVenta;
-    }
-
-    public String getFechaVenta() {
-        return fechaVenta;
-    }
-
-    public void setFechaVenta(String fechaVenta) {
-        this.fechaVenta = fechaVenta;
-    }
-
-    public Cliente getCliente() {
-        return cliente;
-    }
-
-    public void setCliente(Cliente cliente) {
-        this.cliente = cliente;
-    }
-
-    public ArrayList<Producto> getProductos() {
-        return productos;
-    }
-
-    public void setProductos(ArrayList<Producto> productos) {
-        this.productos = productos;
+        for (DetalleVenta detalle : DetalleVenta){
+            Producto aux=detalle.getProducto();
+            int totalLinea = aux.getPrecio()*detalle.getCantidad();
+            System.out.println("Cantidad: "+detalle.getCantidad()+" "+aux.getDescripcion()+"total: "+totalLinea);
+            acumulado=acumulado+totalLinea;
+        }
+        System.out.println("Total NETO: "+acumulado);
+        double iva=acumulado*0.19;
+        System.out.println("IVA: "+iva);
+        System.out.println("Valor total: "+(acumulado+iva));
     }
 }
