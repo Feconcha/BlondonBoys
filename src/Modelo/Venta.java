864e752b58dedbdba7ddde79dddb01021503dccd
Renat0Espinoza <131412695+Renat0Espinoza@users.noreply.github.com>
2023-05-31 08:57:36 -0400

    creacion clase venta

--- src/Modelo/Venta.java
+++ src/Modelo/Venta.java
@@ -1,12 +1,18 @@
 package Modelo;
 
+import java.util.ArrayList;
+
 public class Venta {
     private long codigoVenta;
     private String fechaVenta;
+    private Cliente cliente;
+    private ArrayList<Producto> productos;
 
-    public Venta(long codigoVenta, String fechaVenta) {
+    public Venta(long codigoVenta, String fechaVenta, Cliente cliente, ArrayList<Producto> productos) {
         this.codigoVenta = codigoVenta;
         this.fechaVenta = fechaVenta;
+        this.cliente = cliente;
+        this.productos = productos;
     }
 
     public long getCodigoVenta() {
@@ -23,5 +29,21 @@ public class Venta {
 
     public void setFechaVenta(String fechaVenta) {
         this.fechaVenta = fechaVenta;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public ArrayList<Producto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(ArrayList<Producto> productos) {
+        this.productos = productos;
     }
 }
