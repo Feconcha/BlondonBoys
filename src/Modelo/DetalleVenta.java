be4e09a4fccd492baa9283eea073e6125def8da5
Feconcha <felipe.concha2201@alumnos.ubiobio.cl>
2023-06-09 14:49:01 -0400

    agrega metodos a clase DetalleVenta

--- src/Modelo/DetalleVenta.java
+++ src/Modelo/DetalleVenta.java
@@ -6,4 +6,26 @@ public class DetalleVenta {
     private int cantidad;
     private ArrayList<Venta> ventas;
     private Producto producto;
+
+    public DetalleVenta(int cantidad, Producto producto) {
+        this.cantidad = cantidad;
+        this.producto = producto;
+        ventas = new ArrayList<>();
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
 }
