937a3b860582ddcd953c1568d6aa5c60c73bd762
Renat0Espinoza <131412695+Renat0Espinoza@users.noreply.github.com>
2023-06-20 14:15:48 -0400

    detalle venta actualizado

--- src/Modelo/DetalleVenta.java
+++ src/Modelo/DetalleVenta.java
@@ -1,22 +1,31 @@
 package Modelo;
 
+import java.util.ArrayList;
+
 public class DetalleVenta {
     private int cantidad;
+    private Venta venta;
     private Producto producto;
+
+    public DetalleVenta (int cantidad, Venta venta, Producto producto){
+        this.cantidad = cantidad;
+        this.venta = venta;
+        this.producto = producto;
+    }
 
     public int getCantidad() {
         return cantidad;
     }
 
-    public void setCantidad(int cantidad) {
-        this.cantidad = cantidad;
+    public Venta getVenta() {
+        return venta;
     }
 
     public Producto getProducto() {
         return producto;
     }
 
-    public void setProducto(Producto producto) {
-        this.producto = producto;
+    public String toString(){
+        return producto.getCodigo()+";"+cantidad;
     }
 }
