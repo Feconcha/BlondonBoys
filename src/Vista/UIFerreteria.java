e3832cf53a2b792618e70fedc8b926e59bb62417
Feconcha <felipe.concha2201@alumnos.ubiobio.cl>
2023-06-20 15:14:58 -0400

    modifica CrearVenta

--- src/Vista/UIFerreteria.java
+++ src/Vista/UIFerreteria.java
@@ -4,6 +4,8 @@ import Controlador.ControladorFerreteria;
 import Modelo.Venta;
 
 import java.io.FileNotFoundException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class UIFerreteria {
@@ -50,22 +52,45 @@ public class UIFerreteria {
         System.out.println("Producto creado exitosamente.");
     }
     public void CrearVenta(){
-        String opcion;
+        String opcion="", fecha, rut;
         long codigo;
+        int cantidad;
+        Venta venta = null;
         System.out.println("Ingrese el rut del cliente");
-        String rut = scan.next();
-        Venta venta = controlador.creaVenta(rut);
+        rut = scan.next();
+        System.out.println("Ingrese fecha de la venta en formato dd/MM/yyyy");
+        fecha = scan.next();
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate fechaLD = LocalDate.parse(fecha,formato);
         do{
             System.out.println("Ingrese el codigo del producto");
             codigo = scan.nextLong();
-            if(controlador.añadirVenta(codigo,venta)){
-                System.out.println("¿Quiere comprar mas productos? s/n");
-                opcion = scan.next();
-            }else {
-                System.out.println("No hay mas stock de ese producto");
-                opcion="s";
+            System.out.println("Ingrese la cantidad que quiere llevar");
+            cantidad = scan.nextInt();
+            if(venta==null){
+                try {
+                    venta = controlador.creaVenta(codigo,rut,cantidad, fechaLD);
+                    System.out.println("¿Quiere comprar mas productos? s/n");
+                    opcion = scan.next();
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            }else{
+                if(controlador.añadirAVenta(codigo,venta,cantidad)){
+                    System.out.println("Producto ingresado exitosamente");
+                    System.out.println("¿Quiere comprar mas productos? s/n");
+                    opcion = scan.next();
+                }else{
+                    System.out.println("No hay suficiente stock para realizar la venta");
+                    System.out.println("¿Quiere comprar mas productos? s/n");
+                    opcion = scan.next();
+                }
+
             }
         }while(opcion.equalsIgnoreCase("s"));
+        if(venta!=null){
+            printBoleta(venta);
+        }
     }
 
     public void ListaClientes() {
@@ -135,7 +160,6 @@ public class UIFerreteria {
 
                     break;
                 case 6:
-
                     break;
                 case 7:
                     try {
