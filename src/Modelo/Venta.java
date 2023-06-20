8b8725d675cc7853e93abf72bc2861afec3fc20a
Feconcha <felipe.concha2201@alumnos.ubiobio.cl>
2023-06-20 11:50:50 -0400

    modifica clase venta

--- src/Modelo/Venta.java
+++ src/Modelo/Venta.java
@@ -7,16 +7,15 @@ import java.util.List;
 
 public class Venta {
     private long codigoVenta;
-    private Cliente cliente;
-    private ArrayList<DetalleVenta> detalleVentas;
     private LocalDate fechaVenta;
-
+    private Cliente cliente;
+    private ArrayList<DetalleVenta>detalleVentas;
 
     public Venta(long codigoVenta, LocalDate fechaVenta, Cliente cliente) {
         this.codigoVenta = codigoVenta;
         this.fechaVenta = fechaVenta;
         this.cliente = cliente;
-        detalleVentas= new ArrayList<>();
+        detalleVentas = new ArrayList<>();
     }
 
     public long getCodigoVenta() {
@@ -27,33 +26,69 @@ public class Venta {
         this.codigoVenta = codigoVenta;
     }
 
+    public LocalDate getFechaVenta() {
+        return fechaVenta;
+    }
+
+    public void setFechaVenta(LocalDate fechaVenta) {
+        this.fechaVenta = fechaVenta;
+    }
+
+    public void addProducto(Producto producto, int cantidad){
+        DetalleVenta venta = new DetalleVenta(cantidad,this,producto);
+        detalleVentas.add(venta);
+    }
+
     public Cliente getCliente() {
         return cliente;
+    }
+
+    public ArrayList<DetalleVenta> getDetalleVentas(){
+        return detalleVentas;
+    }
+
+    public int getMontoTotal(){
+        int monto=0;
+        for(DetalleVenta detalleVenta : detalleVentas){
+            monto+= detalleVenta.getProducto().getPrecio()*detalleVenta.getCantidad();
+        }
+        return monto;
     }
 
     public void setCliente(Cliente cliente) {
         this.cliente = cliente;
     }
-    public ArrayList<DetalleVenta>getDetalleVentas(){
-        return detalleVentas;
+
+    public int getCantidadProductos(){
+        int cont=0;
+        for(int i=0; i<detalleVentas.size(); i++){
+            cont+=detalleVentas.get(i).getCantidad();
+        }
+        return cont;
     }
-    public void setDetalleVentas(ArrayList<DetalleVenta> detalleVentas){
-        this.detalleVentas = detalleVentas;
+
+    //
+    public Producto[] getProductos(){
+        ArrayList<Producto> productos = new ArrayList<>();
+        for(DetalleVenta detalleVenta: detalleVentas){
+            if(detalleVenta.getProducto()!=null){
+                productos.add(detalleVenta.getProducto());
+            }
+        }
+        return productos.toArray(new Producto[0]);
     }
-    public LocalDate getFechaVenta() {
-        return fechaVenta;
+
+    public String[] detalleVenta(){
+        String[] listaVenta = new String[detalleVentas.size()];
+        for(int i=0; i<listaVenta.length; i++){
+            Producto producto = detalleVentas.get(i).getProducto();
+            listaVenta[i] = detalleVentas.get(i).getCantidad() + ";" + producto.getDescripcion() + ";" + producto.getPrecio() + ";" + (detalleVentas.get(i).getCantidad()*producto.getPrecio());
+        }
+        return listaVenta;
     }
-    public void addDetalleVetnas(DetalleVenta detalleVenta){
-        detalleVentas.add(detalleVenta);
-    }
-    public void setFechaVenta(LocalDate fechaVenta) {
-        this.fechaVenta = fechaVenta;
-    }
+
     public String toString(){
         DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         return codigoVenta + ";" + cliente.getRut() + ";" + fechaVenta.format(formato);
     }
-
-
-
 }
